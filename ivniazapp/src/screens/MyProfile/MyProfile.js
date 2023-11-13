7ba32efed1698ad7ba77a2d265a39323980fcabd
azulbid <azulbid@gmail.com>
2023-11-13 14:52:39 -0300

    profile entra ok

--- ivniazapp/src/screens/MyProfile/MyProfile.js
+++ ivniazapp/src/screens/MyProfile/MyProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {db, auth } from '../../firebase/config';
-import {TextInput, TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import {TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList} from 'react-native';
 import Post from "../../components/Post/Post";
 
 
