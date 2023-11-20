548bff2fd18992a81f0bb97e3e4c91e611bba6af
azulbid <azulbid@gmail.com>
2023-11-19 22:10:24 -0300

    edit profile y eliminar profile - optativos intentados

--- ivniazapp/src/screens/MyProfile/MyProfile.js
+++ ivniazapp/src/screens/MyProfile/MyProfile.js
@@ -15,7 +15,12 @@ class MyProfile extends Component{
             id: '',
             newPass:"",
             nombreUser: "",
-
+            perfilABorrar: null,
+            edit: false,
+            newUsername: '',
+            newEmail: '',
+            newBio: '',
+            errors: '',
         };
     }
 
@@ -55,7 +60,55 @@ class MyProfile extends Component{
     this.props.navigation.navigate("Login");
     }
 
-    
+    borrarPerfil(item){
+        db.collection('users').doc(this.state.dataUser[0]).delete()
+        .then(()=> {
+            const user = firebase.auth().currentUser;
+            user.delete();
+            alert('El perfil se eliminó exitosamente');
+            this.props.navigation.navigate('Register')
+        })
+            
+        .catch(error => {
+            console.error('error')
+        })
+    }
+
+    confBorrarPerfil(item){
+        this.setState({perfilABorrar: item})
+    }
+
+    finalBorrarPerfil(){
+        this.borrarPerfil(this.state.perfilABorrar);
+        this.setState({ perfilABorrar: null});
+        this.navigation.navigate("Register")
+    }    
+
+    noBorrarPerfil(){
+        this.setState({ perfilABorrar: null});
+    }
+
+    saveChanges() {
+        const { newUsername, newBio } = this.state;
+
+        if (newUsername.trim() === '') {
+            this.setState({ errors: 'El nombre de usuario no puede estar vacío' });
+        } else {
+            db.collection('users').doc(this.state.user.id).update({
+                nombreUser: newUsername,
+                bio: newBio,
+            })
+            .then(() => {
+                this.setState({
+                    edit: false,
+                    errors: '',
+                });
+            })
+            .catch(error => {
+                this.setState({ errors: error.message });
+            });
+        }
+    }
 
     render() {
       console.log('Esto es el Profile')
@@ -79,12 +132,76 @@ class MyProfile extends Component{
                 <TouchableOpacity onPress={() => this.logout()} style={styles.logoutButton}>
                     <Text style={styles.logoutText}>Cerrar sesión</Text>
                 </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => this.setState({ edit: true })} style={styles.button}>
+                    <Text style={styles.buttonText}>Editar Perfil</Text>
+                </TouchableOpacity>
+
+                {this.state.user
+                    ? <View style={styles.container}>
+                        
+                        <View style={styles.container}>
+                            {this.state.edit ? (
+                                <View>
+                                    <Text style={styles.nombreUser}>Nuevo nombre de usuario:</Text>
+                                    <TextInput
+                                        placeholder="Nuevo nombre de usuario"
+                                        onChangeText={(text) => this.setState({ newUsername: text })}
+                                        value={this.state.newUsername}
+                                    />
+                                    
+                                    <Text style={styles.nombreUser}>Nueva bio:</Text>
+                                    <TextInput
+                                       
+                                        placeholder="Nueva bio"
+                                        onChangeText={(text) => this.setState({ newBio: text })}
+                                        value={this.state.newBio}
+                                    />
+                                    <TouchableOpacity style={styles.button} onPress={() => this.saveChanges()}>
+                                        <Text style={styles.buttonText}>Confirmar Cambios</Text>
+                                    </TouchableOpacity>
+                                   
+                                </View>
+                            ) : (
+                                <View>
+                                    <Text style={styles.username}>{this.state.user.data.username}</Text>
+                                    <Text style={styles.email}>{this.state.user.data.owner}</Text>
+                                    <Text style={styles.bio}>{this.state.user.data.bio}</Text>
+                                    <Image style={styles.profileImage} source={{ uri: this.state.dataUser.profileImage }} />
+                                
+                                </View>
+                            )}
+                        </View>
+                    </View>
+                    : <Text> </Text>
+                }
                
-            
+                <TouchableOpacity style={styles.button} onPress={() => this.confBorrarPerfil()}>
+                        <Text>Borrar perfil </Text>
+                </TouchableOpacity>
+
+                {this.state.perfilABorrar !== null ?
+                <View>
+                   <Text>
+                    Querés eliminar el perfil? No podrás recuperarlo
+                </Text>
+
+                <TouchableOpacity style={styles.button} onPress={() => this.finalBorrarPerfil()}>
+                <Text style={styles.textButtonDelete}>Aceptar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button} onPress={() => this.noBorrarPerfil()}>
+                <Text style={styles.textButtonDelete}>Cancelar</Text>
+                </TouchableOpacity>
+                </View>
+                
+                 :
+                null
+            }
             </ScrollView>
             
-  )}}
-
+  )}
+        }
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#fff',
@@ -127,7 +244,13 @@ const styles = StyleSheet.create({
     logoutText: {
         color: '#fff',
         fontWeight: 'bold',
-    }
+    },
+    button: {
+        backgroundColor: '#3498db',
+        padding: 15,
+        alignItems: 'center',
+        borderRadius: 8,
+      }
 
 });
 
